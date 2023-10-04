68bf2742eed7264b5191855c2e661f5b7804597b
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-10-04 01:15:05 -0400

    make single-page site, update contact info (#18)

---
+++ assets/js/utilities.js
@@ -0,0 +1,14 @@
+function scrollToStart(id) {
+    console.log("foo");
+    setTimeout(function() {
+        var element = document.getElementById(id);
+        element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+    }, 200);
+}
+
+function scrollToEnd(id) {
+    setTimeout(function() {
+        var element = document.getElementById(id);
+        element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'end' });
+    }, 200);
+}
