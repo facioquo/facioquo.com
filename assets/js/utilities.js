491e392123faef364aa08425d88b5582b667944c
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2023-10-04 02:28:13 -0400

    fix Jekyll offending JS code (#22)

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -1,12 +1,12 @@
 function scrollToStart(id) {
-  setTimeout(() => {
+  setTimeout(function () {
     var element = document.getElementById(id);
     element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
   }, 200);
 }
 
 function scrollToEnd(id) {
-  setTimeout(() => {
+  setTimeout(function () {
     var element = document.getElementById(id);
     element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'end' });
   }, 200);
